27c6d5b70c2fbedb8ccb588f1bb67bda1b2e25a1
magdy kamal <magdy.kamal.ok@gmail.com>
2020-02-09 02:03:49 +0200

    check if login process working fine

--- LoginDemoRxReduxUITests/LoginViewUITests.swift
+++ LoginDemoRxReduxUITests/LoginViewUITests.swift
@@ -27,7 +27,15 @@ class LoginViewUITests: XCTestCase {
     func launchApp() {
         app.launch()
     }
-   
+    
+    func enterUsernameAndPassword(userName:String, password:String){
+        app.textFields["Username"].tap()
+        app.textFields["Username"].typeText(userName)
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText(password)
+        
+    }
+    
     func testCheckIfTitleLableExists() {
         let travelAppLabel = app.staticTexts["travelapp"]
         XCTAssert(travelAppLabel.exists)
@@ -63,9 +71,33 @@ class LoginViewUITests: XCTestCase {
         XCTAssert(lockButton.exists)
     }
     
+    func testCheckIfLockButtonExists() {
+        app.buttons["unlock"].tap()
+        let lockButton = app.buttons["lock"]
+        XCTAssert(lockButton.exists)
+    }
+    
     func testCheckIfLoginButtonDisabled() {
         let loginBtn = app.buttons["Login"]
         XCTAssertFalse(loginBtn.isEnabled)
     }
     
+    func testIfLoginButtonEnabled() {
+        enterUsernameAndPassword(userName: "FirstName", password: "FirstPassword")
+        let loginBtn = app.buttons["Login"]
+        XCTAssertTrue(loginBtn.isEnabled)
+    }
+    
+    func testCheckIfPasswordIsEncypted() {
+        enterUsernameAndPassword(userName: "FirstName", password: "FirstPassword")
+        let password = app.secureTextFields["Password"]
+        XCTAssert(password.value as! String  == "•••••••••••••")
+    }
+    
+    func testCheckIfUnLockButtonMakePasswordReadable() {
+        enterUsernameAndPassword(userName: "FirstName", password: "FirstPassword")
+        let password = app.textFields["Password"]
+        app.buttons["unlock"].tap()
+        XCTAssert(password.value as! String  == "FirstPassword")
+    }
 }
